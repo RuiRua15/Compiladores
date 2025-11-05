64586d9a94d7667e0a932db5d55f5b1b01b60170
DavidRodrigues <david20rodrigues05@gmail.com>
2025-11-05 15:24:46 +0000

    Delete COMP_TP1_GRUPO_6 directory

--- COMP_TP1_GRUPO_6/example/Main.adb
+++
@@ -1,7 +0,0 @@
-procedure Main is
-   X : Integer := 0;
-begin
-   while X < 5 loop
-      X := X + 1;
-   end loop;
-end Main;
--- COMP_TP1_GRUPO_6/src/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "lexer.h"
-#include "ast.h"
-
-ASTNode *parse_program(void);
-
-#endif
